908d2ddf47b79e208f3ec9f7633f4f7e2acb1bcb
mangelrosasreyes <mangelrosasreyes@gmail.com>
2017-06-03 12:52:10 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,15 +13,20 @@ function setup() {
 
 function draw() {
   frameRate (20);
-  rojo = random (0,255);
-  verde = random (0,255);
-  azul = random (0,255);
 
-  background (255,0,0); //se colorea el canvas
-  fill (rojo, verde, azul); //se colorea los elementos
-  triangle(30, 75, 58, 20, 86, 75);
-  //ellipse(mouseX,mouseY,200,200); //para que el elemento siga al mouse
-  ellipse(300,150,200,200);
-  ellipse(500,150,200,200);
-  //x = x + 2;
-}
+  textSize(16);
+textAlign(RIGHT);
+text("POWER OF LOVE", 50, 30);
+textAlign(CENTER);
+
+
+  if(mouseIsPressed == true){
+    rojo = random (0,255);
+    verde = random (0,255);
+    azul = random (0,255);
+  }
+  else{
+    rojo = 255;
+    verde = 255;
+    azul = 255;
+  }
